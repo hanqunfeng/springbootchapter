aaa63156a7f99f0894534800096cae72410a4258
hanqf <hanqf2008@163.com>
2020-03-13 14:47:08 +0800

    补充数据库脚本

---
+++ doc/sql/mybatis.sql
@@ -0,0 +1,26 @@
+CREATE TABLE `address` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `province` varchar(255) COLLATE utf8mb4_bin DEFAULT NULL,
+  `city` varchar(255) COLLATE utf8mb4_bin DEFAULT NULL,
+  `userId` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+CREATE TABLE `books` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `bookName` varchar(255) COLLATE utf8mb4_bin DEFAULT NULL,
+  `price` double(10,2) DEFAULT NULL,
+  `totalPage` int(10) DEFAULT NULL,
+  `userId` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+
+CREATE TABLE `user` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键ID',
+  `name` varchar(30) COLLATE utf8mb4_bin DEFAULT NULL COMMENT '姓名',
+  `age` int(11) DEFAULT NULL COMMENT '年龄',
+  `email` varchar(50) COLLATE utf8mb4_bin DEFAULT NULL COMMENT '邮箱',
+  `del` varchar(1) COLLATE utf8mb4_bin DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
