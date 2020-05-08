f6ef9661dfcd232f60dfa5abfe42f9d3b6e8b4a2
hanqf <hanqf2008@163.com>
2020-05-08 15:54:44 +0800

    chapter28 WebFlux redis

--- chapter28/web-flux-demo/src/main/java/com/example/redis/controller/RedisDemoController.java
+++ chapter28/web-flux-demo/src/main/java/com/example/redis/controller/RedisDemoController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.ReactiveHashOperations;
 import org.springframework.data.redis.core.ReactiveStringRedisTemplate;
 import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -40,4 +41,28 @@ public class RedisDemoController {
         Mono<String> hval = reactiveHashOperations.get("USER_HS", String.valueOf(id));
         return hval.map(e -> JSON.parseObject(e, User.class));
     }
+
+    @GetMapping("/all")
+    public Flux<User> findAll() {
+        ReactiveHashOperations reactiveHashOperations = reactiveStringRedisTemplate.opsForHash();
+        //先获取hash中的全部key
+        Flux<String> hkeys = reactiveHashOperations.keys("USER_HS");
+
+        return hkeys.flatMap(id -> {
+            //通过每一个key获取对应的值，即json字符串，并转换为User对象
+            Mono<String> user_hs = reactiveHashOperations.get("USER_HS", String.valueOf(id));
+            return user_hs.map(e -> JSON.parseObject(e, User.class));
+        });
+    }
+
+    @GetMapping("/all2")
+    public Flux<User> findAll2() {
+        ReactiveHashOperations reactiveHashOperations = reactiveStringRedisTemplate.opsForHash();
+        //先获取hash中的全部key
+        Flux<String> hvalues = reactiveHashOperations.values("USER_HS");
+
+        return hvalues.map(value -> JSON.parseObject(value, User.class));
+    }
+
+
 }
