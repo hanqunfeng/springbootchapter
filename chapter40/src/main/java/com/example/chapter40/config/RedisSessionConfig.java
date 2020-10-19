f87fb0c99d2aee9c29646a84c04a76d93d31944b
hanqf <hanqf2008@163.com>
2020-10-19 18:09:29 +0800

    redis session

--- chapter40/src/main/java/com/example/chapter40/config/RedisSessionConfig.java
+++ chapter40/src/main/java/com/example/chapter40/config/RedisSessionConfig.java
@@ -3,7 +3,11 @@ package com.example.chapter40.config;/**
  */
 
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 /**
@@ -13,6 +17,20 @@ import org.springframework.session.data.redis.config.annotation.web.http.EnableR
 @Configuration
 //设置session的过期时间为60分钟
 @EnableRedisHttpSession(maxInactiveIntervalInSeconds = 60 * 60)
+@Slf4j
 public class RedisSessionConfig {
+
+    /**
+     * <h2>设置session值存储方式，默认jdk序列化方式，这里改为json</h2>
+     * Created by hanqf on 2020/10/19 18:07. <br>
+     *
+     * @return org.springframework.data.redis.serializer.RedisSerializer&lt;java.lang.Object&gt;
+     * @author hanqf
+     */
+    @Bean("springSessionDefaultRedisSerializer")
+    public RedisSerializer<Object> defaultRedisSerializer() {
+        log.debug("自定义Redis Session序列化加载成功");
+        return new GenericJackson2JsonRedisSerializer();
+    }
 
 }
