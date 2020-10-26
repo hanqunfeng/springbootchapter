a800dc1e35691b9318eba63b017d4c12c19965f4
hanqf <hanqf2008@163.com>
2020-10-26 18:16:32 +0800

    Springboot chapter43 分页数据缓存

--- chapter43/src/main/java/com/example/service/impl/ArticalServcieImpl.java
+++ chapter43/src/main/java/com/example/service/impl/ArticalServcieImpl.java
@@ -23,26 +23,26 @@ import java.util.List;
  */
 
 @Service
-@Transactional(rollbackFor = {Exception.class })
+@Transactional(rollbackFor = {Exception.class})
 //@Transactional(rollbackOn = {Exception.class})
 //该注解的作用是表明此类上所有方法上的事务都是CGLib方式代理的。实际上spring会自动判断其代理方式。
 //@Scope(proxyMode = ScopedProxyMode.TARGET_CLASS)
 //缓存key的前缀
 @CacheConfig(cacheNames = "articalCache")
-public class ArticalServcieImpl  implements ArticalServcie {
+public class ArticalServcieImpl implements ArticalServcie {
     @Autowired
     private ArticalRepository articalRepository;
 
 
     //@Transactional(value = Transactional.TxType.SUPPORTS)
     @Override
-    @Transactional(readOnly = true,propagation = Propagation.SUPPORTS)
+    @Transactional(readOnly = true, propagation = Propagation.SUPPORTS)
     //先查询缓存，如果存在则返回缓存的值，不存在则执行方法，并将返回值存储到缓存
     @Cacheable(key = "'ArticalServcieImpl.findById_'+#id")
-    public Artical findById(Long id){
+    public Artical findById(Long id) {
         Artical artical = articalRepository.findById(id).orElse(null);
         if (artical == null) {
-            throw new CustomException(CustomExceptionType.USER_INPUT_ERROR,"给定id文章不存在!");
+            throw new CustomException(CustomExceptionType.USER_INPUT_ERROR, "给定id文章不存在!");
         }
         return artical;
     }
@@ -50,23 +50,22 @@ public class ArticalServcieImpl  implements ArticalServcie {
 
     //@Transactional(value = Transactional.TxType.SUPPORTS)
     @Override
-    @Transactional(readOnly = true,propagation = Propagation.SUPPORTS)
+    @Transactional(readOnly = true, propagation = Propagation.SUPPORTS)
     @Cacheable(key = "'ArticalServcieImpl.findAll'")
-    public List<Artical> findAll(){
+    public List<Artical> findAll() {
         return articalRepository.findAll();
     }
-
 
 
     //清除掉指定key的缓存
     @Override
     @Caching(evict = {
-            @CacheEvict(key="'ArticalServcieImpl.findAll'"),
-            @CacheEvict(cacheNames = "articalCachePages",allEntries = true)
+            @CacheEvict(key = "'ArticalServcieImpl.findAll'"),
+            @CacheEvict(cacheNames = "articalCachePages", allEntries = true)
     })
-    public Artical save(Artical artical){
+    public Artical save(Artical artical) {
         if (artical.getId() != null) {
-            throw new CustomException(CustomExceptionType.USER_INPUT_ERROR,"新增操作，文章Id不能赋值，修改操作请使用put method!");
+            throw new CustomException(CustomExceptionType.USER_INPUT_ERROR, "新增操作，文章Id不能赋值，修改操作请使用put method!");
         }
         artical = articalRepository.save(artical);
         return artical;
@@ -75,14 +74,14 @@ public class ArticalServcieImpl  implements ArticalServcie {
     //一次可以配置多个类型
     @Override
     @Caching(evict = {
-            @CacheEvict(key="'ArticalServcieImpl.findAll'"),
-            @CacheEvict(cacheNames = "articalCachePages",allEntries = true)
-    },put = {
+            @CacheEvict(key = "'ArticalServcieImpl.findAll'"),
+            @CacheEvict(cacheNames = "articalCachePages", allEntries = true)
+    }, put = {
             @CachePut(key = "'ArticalServcieImpl.findById_'+#artical.id")
     })
-    public Artical update(Artical artical){
+    public Artical update(Artical artical) {
         if (artical.getId() == null) {
-            throw new CustomException(CustomExceptionType.USER_INPUT_ERROR,"修改操作，文章Id不能为空!");
+            throw new CustomException(CustomExceptionType.USER_INPUT_ERROR, "修改操作，文章Id不能为空!");
         }
         return articalRepository.save(artical);
     }
@@ -90,24 +89,34 @@ public class ArticalServcieImpl  implements ArticalServcie {
     //一次可以配置多个类型
     @Override
     @Caching(evict = {
-            @CacheEvict(key="'ArticalServcieImpl.findAll'"),
+            @CacheEvict(key = "'ArticalServcieImpl.findAll'"),
             @CacheEvict(key = "'ArticalServcieImpl.findById_'+#id"),
-            @CacheEvict(cacheNames = "articalCachePages",allEntries = true)
+            @CacheEvict(cacheNames = "articalCachePages", allEntries = true)
     })
-    public void deleteById(Long id){
+    public void deleteById(Long id) {
         articalRepository.deleteById(id);
     }
 
+    /**
+     * <h2>这里返回自定义类型，是因为Page没有无参的构造函数，缓存后取出时不能从json转换为对象</h2>
+     * Created by hanqf on 2020/10/26 18:15. <br>
+     *
+     * @param artical
+     * @param page
+     * @param sort
+     * @return com.example.views.PageList&lt;com.example.model.Artical&gt;
+     * @author hanqf
+     */
     @Override
-    @Transactional(readOnly = true,propagation = Propagation.SUPPORTS)
-    @Cacheable(cacheNames = "articalCachePages",key="'ArticalServcieImpl.pages_' + #artical + #page + #sort")
+    @Transactional(readOnly = true, propagation = Propagation.SUPPORTS)
+    @Cacheable(cacheNames = "articalCachePages", key = "'ArticalServcieImpl.pages_' + #artical + #page + #sort")
     public PageList<Artical> findAll(Artical artical, CustomPage page, CustomSort sort) {
         PageList<Artical> list = new PageList<>();
         PageRequest pageRequest = PageRequest.of(page.getIndex(), page.getSize(), Sort.by(sort.getOrder()));
         //CONTAINING: 字符串类型模糊匹配，不走索引的，尽量不要使用
         //STARTING: 以字符串开头
         Page<Artical> articalPage = articalRepository.findAll(Example.of(artical, ExampleMatcher.matching().withStringMatcher(ExampleMatcher.StringMatcher.STARTING)), pageRequest);
-        if(articalPage.hasContent()){
+        if (articalPage.hasContent()) {
             list.setTotal(articalPage.getTotalElements());
             list.setData(articalPage.getContent());
         }
