46f52876974571429189b4e6e38c1b4de0192a5d
hanqf <hanqf2008@163.com>
2020-11-08 00:03:37 +0800

    chapter48 OAuth2 Client单点登录，增加对资源服务器的访问方法

---
+++ chapter48/oauth2-client-demo/src/main/java/com/example/oauth2clientdemo/controller/ResController.java
@@ -0,0 +1,32 @@
+package com.example.oauth2clientdemo.controller;
+
+import com.example.oauth2clientdemo.exception.AjaxResponse;
+import com.example.oauth2clientdemo.security.OAuth2ResourceRestTemplateUtil;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <h1>获取资源服务器数据</h1>
+ * Created by hanqf on 2020/11/7 22:47.
+ */
+
+@RestController
+@RequestMapping("/res")
+public class ResController {
+
+
+    /**
+     * 获取资源服务器的数据
+    */
+    @RequestMapping("/res1")
+    public AjaxResponse getRes(){
+        return OAuth2ResourceRestTemplateUtil.getRestTemplate().getForObject("http://localhost:8081/res/res1", AjaxResponse.class);
+    }
+
+    @RequestMapping("/user")
+    public AjaxResponse getUser(){
+        return OAuth2ResourceRestTemplateUtil.getRestTemplate().postForObject("http://localhost:8081/user",null, AjaxResponse.class);
+    }
+
+
+}
---
+++ chapter48/oauth2-client-demo/src/main/java/com/example/oauth2clientdemo/security/OAuth2ResourceRestTemplateUtil.java
@@ -0,0 +1,96 @@
+package com.example.oauth2clientdemo.security;
+
+import com.example.oauth2clientdemo.exception.CustomException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.client.ClientHttpRequestExecution;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationDetails;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * <h1>获取资源服务器数据工具类</h1>
+ * Created by hanqf on 2020/11/7 23:17.
+ */
+
+@Slf4j
+public class OAuth2ResourceRestTemplateUtil {
+
+    public static RestTemplate getRestTemplate(){
+        //设置连接超时和读取超时时间
+        SimpleClientHttpRequestFactory factory=new SimpleClientHttpRequestFactory();
+        factory.setConnectTimeout(5000);
+        factory.setReadTimeout(5000);
+        RestTemplate restTemplate = new RestTemplate(factory);
+        //拦截器
+        List<ClientHttpRequestInterceptor> list = new ArrayList<>();
+        list.add(new OAuth2ResourceRestTemplateUtil.RetryIntercepter()); //重试拦截器
+        list.add(new OAuth2ResourceRestTemplateUtil.HeadersLoggingInterceper()); //header拦截器
+        restTemplate.setInterceptors(list);
+        return restTemplate;
+    }
+
+    /**
+     * <p>重试拦截器</p>
+     */
+    private static class RetryIntercepter implements ClientHttpRequestInterceptor {
+
+        private int maxRetry = 3;//最大重试次数，默认3次
+        private int retryNum = 0;
+
+        public RetryIntercepter() {
+
+        }
+
+        public RetryIntercepter(int maxRetry) {
+            this.maxRetry = maxRetry;
+        }
+
+        @Override
+        public ClientHttpResponse intercept(HttpRequest httpRequest, byte[] bytes, ClientHttpRequestExecution clientHttpRequestExecution) throws IOException {
+            ClientHttpResponse response = clientHttpRequestExecution.execute(httpRequest, bytes);
+            if (!response.getStatusCode().equals(HttpStatus.OK) && retryNum < maxRetry) {
+                retryNum++;
+                response = clientHttpRequestExecution.execute(httpRequest, bytes);
+
+            }
+            return response;
+        }
+    }
+
+    /**
+     * <p>Headers拦截器</p>
+     */
+    private static class HeadersLoggingInterceper implements ClientHttpRequestInterceptor {
+
+        @Override
+        public ClientHttpResponse intercept(HttpRequest httpRequest, byte[] bytes, ClientHttpRequestExecution clientHttpRequestExecution) throws IOException,CustomException {
+
+            try {
+                OAuth2Authentication authentication = (OAuth2Authentication) SecurityContextHolder.getContext().getAuthentication();
+                OAuth2AuthenticationDetails details = (OAuth2AuthenticationDetails) authentication.getDetails();
+                String tokenType = details.getTokenType();
+                String tokenValue = details.getTokenValue();
+                httpRequest.getHeaders().add("authorization", tokenType + " " + tokenValue);
+            }catch (Exception e){
+                throw new CustomException(HttpStatus.FORBIDDEN,e.getMessage(),e.getClass().getName());
+            }
+            log.info(String.format("请求地址: %s", httpRequest.getURI()));
+            log.info(String.format("请求头信息: %s", httpRequest.getHeaders()));
+            ClientHttpResponse response = clientHttpRequestExecution.execute(httpRequest, bytes);
+            log.info(String.format("响应头信息: %s", response.getHeaders()));
+            return response;
+        }
+    }
+
+
+}
