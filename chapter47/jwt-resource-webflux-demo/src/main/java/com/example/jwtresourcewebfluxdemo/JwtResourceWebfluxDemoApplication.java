42972f6c607eaba85e338101e0998a790a471dfc
hanqf <hanqf2008@163.com>
2020-11-20 12:55:09 +0800

    chapter47 基于反应式的JWT资源服务器

---
+++ chapter47/jwt-resource-webflux-demo/src/main/java/com/example/jwtresourcewebfluxdemo/JwtResourceWebfluxDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.jwtresourcewebfluxdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtResourceWebfluxDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtResourceWebfluxDemoApplication.class, args);
+	}
+
+}
---
+++ chapter47/jwt-resource-webflux-demo/src/test/java/com/example/jwtresourcewebfluxdemo/JwtResourceWebfluxDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jwtresourcewebfluxdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtResourceWebfluxDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
