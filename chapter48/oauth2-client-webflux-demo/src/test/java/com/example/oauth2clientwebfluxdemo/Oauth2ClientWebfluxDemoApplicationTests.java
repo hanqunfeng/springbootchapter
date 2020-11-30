728ab7315a7dd673bc819044a8b9a9c002512a39
hanqf <hanqf2008@163.com>
2020-11-30 18:23:51 +0800

    oatuh2 client server webflux

---
+++ chapter48/oauth2-client-webflux-demo/src/test/java/com/example/oauth2clientwebfluxdemo/Oauth2ClientWebfluxDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oauth2clientwebfluxdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2ClientWebfluxDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
