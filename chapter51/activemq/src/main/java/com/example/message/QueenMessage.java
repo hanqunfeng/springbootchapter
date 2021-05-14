c81f95261981c8a57d342658be5db88056e621c3
hanqf <hanqf2008@163.com>
2021-05-14 17:20:11 +0800

    activemq示例

--- chapter51/activemq/src/main/java/com/example/message/QueenMessage.java
+++ chapter51/activemq/src/main/java/com/example/message/QueenMessage.java
@@ -8,6 +8,10 @@ import java.io.Serializable;
 /**
  * <h1>消息对象</h1>
  * Created by hanqf on 2021/5/14 14:55.
+ *
+ * JMS规范中的消息类型包括TextMessage、MapMessage、ObjectMessage、BytesMessage、和StreamMessage等五种。
+ * ActiveMQ也有对应的实现，基于springboot，可以将任意类型作为消息体，springboot在调用时会进行相应的类型转换。
+ * 本例使用的实际上就是ObjectMessage，这里注意要实现Serializable接口
  */
 
 
