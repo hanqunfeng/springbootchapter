4d82083d9603437d3379582e3a0ae6b8e3312cad
hanqunfeng <hanqf2008@163.com>
2022-07-12 16:58:54 +0800

    html抓取

--- chapter59/src/main/java/com/example/Chapter59Application.java
+++ chapter59/src/main/java/com/example/Chapter59Application.java
@@ -1,10 +1,57 @@
 package com.example;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.ssl.SSLContexts;
+import org.apache.http.ssl.TrustStrategy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
 
+import javax.net.ssl.SSLContext;
+
+@Slf4j
 @SpringBootApplication
 public class Chapter59Application {
+
+    @Primary
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate(generateHttpsRequestFactory());
+    }
+
+    public HttpComponentsClientHttpRequestFactory generateHttpsRequestFactory() {
+        try {
+            TrustStrategy acceptingTrustStrategy = (x509Certificates, authType) -> true;
+            SSLContext sslContext = SSLContexts.custom().loadTrustMaterial(null, acceptingTrustStrategy).build();
+            SSLConnectionSocketFactory connectionSocketFactory =
+                    new SSLConnectionSocketFactory(sslContext, new NoopHostnameVerifier());
+
+            HttpClientBuilder httpClientBuilder = HttpClients.custom();
+            httpClientBuilder.setSSLSocketFactory(connectionSocketFactory);
+            CloseableHttpClient httpClient = httpClientBuilder.build();
+            HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+            factory.setHttpClient(httpClient);
+            factory.setConnectTimeout(10 * 1000);
+            factory.setReadTimeout(30 * 1000);
+            return factory;
+        } catch (Exception e) {
+            log.error("创建HttpsRestTemplate失败", e);
+            throw new RuntimeException("创建HttpsRestTemplate失败", e);
+        }
+
+    }
+
+
+
+
 
     public static void main(String[] args) {
         SpringApplication.run(Chapter59Application.class, args);
