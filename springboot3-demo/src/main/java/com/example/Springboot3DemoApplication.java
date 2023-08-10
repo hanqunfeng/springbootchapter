a0ad9ed00f861d3009b843d0ac45761c744f4525
hanqunfeng <hanqf2008@163.com>
2023-08-10 15:03:46 +0800

    springboot3 demo

---
+++ springboot3-demo/src/main/java/com/example/Springboot3DemoApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot3DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot3DemoApplication.class, args);
+	}
+
+}
---
+++ springboot3-demo/src/test/java/com/example/Springboot3DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot3DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
