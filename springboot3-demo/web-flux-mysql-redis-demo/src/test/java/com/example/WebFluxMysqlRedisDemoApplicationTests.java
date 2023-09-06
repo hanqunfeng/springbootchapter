0c16dd8586129bf7b477cc26b1cb8ce7c1cd931b
hanqunfeng <hanqf2008@163.com>
2023-09-06 17:45:24 +0800

    springboot3 r2dbc mysql 多数据源

--- springboot3-demo/web-flux-mysql-redis-demo/src/test/java/com/example/WebFluxMysqlRedisDemoApplicationTests.java
+++ springboot3-demo/web-flux-mysql-redis-demo/src/test/java/com/example/WebFluxMysqlRedisDemoApplicationTests.java
@@ -1,13 +1,39 @@
 package com.example;
 
+import com.example.mysql.SysUser;
+import com.example.mysql.SysUserRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.reactive.TransactionalOperator;
+import reactor.test.StepVerifier;
+
+import java.util.UUID;
 
 @SpringBootTest
 class WebFluxMysqlRedisDemoApplicationTests {
 
+	@Autowired
+	private TransactionalOperator transactionalOperator;
+
+	@Autowired
+	private SysUserRepository sysUserRepository;
+
 	@Test
-	void contextLoads() {
+	void testAddSysUser() {
+		SysUser sysUser = new SysUser();
+		sysUser.setId(UUID.randomUUID().toString());
+		sysUser.setUsername("testAddSysUser");
+		sysUser.setPassword("testAddSysUser");
+		sysUser.setEnable(true);
+		sysUserRepository.addSysUser(sysUser)
+				.as(transactionalOperator::transactional)
+				.log()
+				.as(StepVerifier::create)
+				.expectNextCount(1)
+				.verifyComplete();
+
 	}
+
 
 }
